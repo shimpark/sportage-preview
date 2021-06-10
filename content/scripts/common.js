2096273943bc6b91fc073cab074e8178264f41e5
shimpark <shimpark@gmail.com>
2021-06-10 09:54:31 +0900

    init.

---
+++ content/scripts/common.js
@@ -0,0 +1,59 @@
+//media query matches value
+var mqWeb = window.matchMedia("screen and (min-width: 1376px)");
+var mqPad = window.matchMedia("screen and (max-width: 991.98px)");
+var mqMobile = window.matchMedia("screen and (max-width: 576.98px)");
+
+//open modal event
+function openModal(modalID) {
+    var modalArea = $(".modal-area");
+    var thisModal = $("#" + modalID)
+
+    $("body").addClass("full");
+    modalArea.not(thisModal).hide();
+    thisModal.show();
+}
+//close modal event
+function closeModal(modalID) {
+    var thisModal = $("#" + modalID);
+
+    $("body").removeClass("full");
+    thisModal.hide();
+}
+//close modal event - when click backgrouond
+function clickBackmodal() {
+    var modalArea = $(".modal-area");
+    var modalWrap = $("[class^=modal-wrap__]");
+
+    modalArea.on("click", function(e){
+        if (!$(e.target).closest(modalWrap).length) {
+            $("body").removeClass("full");
+            $(this).fadeOut(150);
+        }
+    });
+}
+
+//show & hide modal history event
+function showHistory() {
+    var historyCont = $(".modal-history-cont");
+    var historyTab = $(".modal-history-tab");
+    
+    historyTab.on("click", function(e) {
+        e.preventDefault();
+
+        var thisHistoryData = $(this).attr("data-history");
+        var thisHistory = $("#" + thisHistoryData);
+
+        $(".modal-history-tab-list li").removeClass("active");
+        $(this).parents("li").addClass("active");
+
+        historyCont.not(thisHistory).hide();
+        thisHistory.show();
+    });
+}
+
+//delete center event
+function deleteCenter(deleteBtn) {
+    setTimeout(function(){
+        $(deleteBtn).closest("li").remove();
+    }, 50);
+}
